7105951131d66604a1f46218fb4fa47e476b0d03
Tal Aroh <tal4884@gmail.com>
2017-07-16 20:52:55 +0300

    initial app commit

---
+++ TechNews/app/src/androidTest/java/com/example/arochta/technews/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.example.arochta.technews;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.arochta.technews", appContext.getPackageName());
+    }
+}
